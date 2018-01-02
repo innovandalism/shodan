8ec8da2df39ca017ec0082045b356eb9f397f554
Aurelia <aurelia@schittler.dev>
2018-01-02 04:52:37 +0100

    Fix: rename version to core in buildfile

--- bin/shodan/main.go
+++ bin/shodan/main.go
@@ -4,11 +4,11 @@ import (
 	"github.com/innovandalism/shodan"
 
 	// module imports
+	_ "github.com/innovandalism/shodan/modules/core"
 	_ "github.com/innovandalism/shodan/modules/gmtools"
 	_ "github.com/innovandalism/shodan/modules/greet"
 	_ "github.com/innovandalism/shodan/modules/log"
 	_ "github.com/innovandalism/shodan/modules/oauth"
-	_ "github.com/innovandalism/shodan/modules/version"
 	_ "github.com/innovandalism/shodan/modules/webui"
 )
 
